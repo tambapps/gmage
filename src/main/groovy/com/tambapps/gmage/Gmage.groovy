d73985a59d3141397a4e0a45831ac495cea76e62
Nelson <nfonkoua@weborama.com>
2020-12-16 23:42:12 +0100

    created Gmage class

--- src/main/groovy/com/tambapps/gmage/Gmage.groovy
+++ src/main/groovy/com/tambapps/gmage/Gmage.groovy
@@ -1,4 +1,29 @@
 package com.tambapps.gmage
 
+import com.tambapps.gmage.pixel.Pixel
+
 class Gmage {
+
+  final int width
+  final int height
+  private final Pixel[] pixels
+
+  Gmage(int width, int height, Pixel[] pixels) {
+    this.width = width
+    this.height = height
+    if (pixels.size() != width * height) {
+      throw new IllegalArgumentException('pixels should have a size of width * height')
+    }
+    this.pixels = new Pixel[pixels.size()]
+    System.arraycopy(pixels, 0, this.pixels, 0, pixels.size())
+  }
+
+  Gmage(int width, int height) {
+    this.width = width
+    this.height = height
+    this.pixels = new Pixel[pixels.size()]
+    for (i in pixels.indices) {
+      pixels[i] = new Pixel()
+    }
+  }
 }
---
+++ src/main/groovy/com/tambapps/gmage/pixel/Pixel.groovy
@@ -0,0 +1,8 @@
+package com.tambapps.gmage.pixel
+
+class Pixel {
+    int r
+    int g
+    int b
+    int a
+}
