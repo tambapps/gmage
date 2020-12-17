0c4b3a2c84854a5853f70c9641fd0acb78639221
Nelson <nfonkoua@weborama.com>
2020-12-17 22:27:04 +0100

    added one test

--- gmage-desktop/src/test/java/com/tambapps/gmage/GmageTest.groovy
+++ gmage-desktop/src/test/java/com/tambapps/gmage/GmageTest.groovy
@@ -1,10 +1,17 @@
 package com.tambapps.gmage
 
+import com.tambapps.gmage.pixel.Pixel
+
 class GmageTest extends GroovyTestCase {
 
-
-    void test() {
-        Gmage gmage = new Gmage(2, 2)
-        assertNotNull(gmage)
-    }
+  void testParseStream() {
+    Gmage gmage = GmageDecoder.decode(GmageTest.class.getResource("/ronflex.jpg"))
+    assertNotNull(gmage)
+    Pixel pixel = gmage[0,0]
+    assertNotNull(pixel)
+    assertEquals(0xff, pixel.r)
+    assertEquals(0xff, pixel.g)
+    assertEquals(0xff, pixel.b)
+    assertEquals(0xff, pixel.a)
+  }
 }
