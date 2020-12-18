9b66f4e0647a2d997857654c634d69e857e307c8
Nelson <nfonkoua@weborama.com>
2020-12-18 20:28:58 +0100

    added tests

--- gmage-core/src/test/java/com/tambapps/gmage/pixel/PixelTest.groovy
+++ gmage-core/src/test/java/com/tambapps/gmage/pixel/PixelTest.groovy
@@ -11,6 +11,7 @@ class PixelTest extends GroovyTestCase {
         assertEquals(0, pixel.getBlue())
         assertEquals(0x000000, pixel.getRGB())
         assertEquals(0xff000000, pixel.getARGB())
+        assertEquals(0xff000000, Pixel.toARGB(255, 0, 0, 0))
     }
 
     void testWhitePixel() {
@@ -21,6 +22,7 @@ class PixelTest extends GroovyTestCase {
         assertEquals(255, pixel.getBlue())
         assertEquals(0xffffff, pixel.getRGB())
         assertEquals(0xffffffff, pixel.getARGB())
+        assertEquals(0xffffffff, pixel.getARGB(), Pixel.toARGB(255, 255, 255, 255))
     }
 
     void testRedPixel() {
@@ -31,6 +33,7 @@ class PixelTest extends GroovyTestCase {
         assertEquals(0, pixel.getBlue())
         assertEquals(0xff0000, pixel.getRGB())
         assertEquals(0xffff0000, pixel.getARGB())
+        assertEquals(0xffff0000, Pixel.toARGB(255, 255, 0, 0))
     }
 
     void testGreenPixel() {
@@ -41,6 +44,7 @@ class PixelTest extends GroovyTestCase {
         assertEquals(0, pixel.getBlue())
         assertEquals(0x00ff00, pixel.getRGB())
         assertEquals(0xff00ff00, pixel.getARGB())
+        assertEquals(0xff00ff00, Pixel.toARGB(255, 0, 255, 0))
     }
 
     void testBluePixel() {
@@ -51,5 +55,33 @@ class PixelTest extends GroovyTestCase {
         assertEquals(255, pixel.getBlue())
         assertEquals(0x0000ff, pixel.getRGB())
         assertEquals(0xff0000ff, pixel.getARGB())
+        assertEquals(0xff0000ff, Pixel.toARGB(255, 0, 0, 255))
+    }
+
+    void testSetters() {
+        Pixel pixel = Pixel.black()
+        pixel.setRed(255)
+        assertEquals(Pixel.of(0xff0000), pixel)
+        pixel.setGreen(255)
+        assertEquals(Pixel.of(0xffff00), pixel)
+        pixel.setBlue(255)
+        assertEquals(Pixel.of(0xffffff), pixel)
+        pixel.setAlpha(0)
+        assertEquals(Pixel.of(0xffffff, 0), pixel)
+        pixel.setRGB(0x123456)
+        assertEquals(Pixel.of(0x123456, 0), pixel)
+        pixel.setARGB(0x12345678)
+        assertEquals(Pixel.of(0x345678, 0x12), pixel)
+    }
+
+
+    void testValuesOutOfBounds() {
+        def pixel = Pixel.black()
+        pixel.red = 12345
+        assertEquals(255, pixel.red)
+        pixel.green = 12345
+        assertEquals(255, pixel.green)
+        pixel.blue = 12345
+        assertEquals(255, pixel.blue)
     }
 }
