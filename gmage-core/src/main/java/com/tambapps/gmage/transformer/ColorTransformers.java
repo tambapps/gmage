8e7a64d4006001905c2290cf942d9e144ffd3840
Nelson <nfonkoua@weborama.com>
2020-12-20 12:34:27 +0100

    simplified transformers

--- gmage-core/src/main/java/com/tambapps/gmage/transformer/ColorTransformers.java
+++ gmage-core/src/main/java/com/tambapps/gmage/transformer/ColorTransformers.java
@@ -8,15 +8,12 @@ public final class ColorTransformers {
     throw new RuntimeException("You can't touch this");
   }
 
-  static final ColorTransformer GRAY_SCALE = (Color c) -> {
-    int gray = (c.getRed() + c.getGreen() + c.getBlue()) / 3;
-    return new Color(c.getAlpha(), gray, gray, gray);
-  };
+  static final ColorTransformer GRAY_SCALE = Color::grayScale;
 
-  static final ColorTransformer RED_FILTER = (Color c) -> new Color(c.getArgb() & 0xffff0000);
+  static final ColorTransformer RED_FILTER = Color.RED::and;
 
-  static final ColorTransformer GREEN_FILTER = (Color c) -> new Color(c.getArgb() & 0xff00ff00);
+  static final ColorTransformer GREEN_FILTER = Color.GREEN::and;
 
-  static final ColorTransformer BLUE_FILTER = (Color c) -> new Color(c.getArgb() & 0xff0000ff);
+  static final ColorTransformer BLUE_FILTER = Color.BLUE::and;
 
 }
