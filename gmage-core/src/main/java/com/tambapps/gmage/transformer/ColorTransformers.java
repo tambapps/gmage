d898c32526ad9792ed186ebdf8315525eb68b647
Nelson <nfonkoua@weborama.com>
2020-12-20 12:49:44 +0100

    created static functions

--- gmage-core/src/main/java/com/tambapps/gmage/transformer/ColorTransformers.java
+++ gmage-core/src/main/java/com/tambapps/gmage/transformer/ColorTransformers.java
@@ -8,12 +8,31 @@ public final class ColorTransformers {
     throw new RuntimeException("You can't touch this");
   }
 
-  static final ColorTransformer GRAY_SCALE = Color::grayScale;
+  private static final ColorTransformer GRAY_SCALE = Color::grayScale;
 
-  static final ColorTransformer RED_FILTER = Color.RED::and;
+  private static final ColorTransformer RED_FILTER = Color.RED::and;
 
-  static final ColorTransformer GREEN_FILTER = Color.GREEN::and;
+  private static final ColorTransformer GREEN_FILTER = Color.GREEN::and;
 
-  static final ColorTransformer BLUE_FILTER = Color.BLUE::and;
+  private static final ColorTransformer BLUE_FILTER = Color.BLUE::and;
 
+  public static ColorTransformer grayScale() {
+    return GRAY_SCALE;
+  }
+
+  public static ColorTransformer redFilter() {
+    return RED_FILTER;
+  }
+
+  public static ColorTransformer greenFilter() {
+    return GREEN_FILTER;
+  }
+
+  public static ColorTransformer blueFilter() {
+    return BLUE_FILTER;
+  }
+
+  public static ColorTransformer duoTone(Color color) {
+    return (c) -> c.grayScale().and(color);
+  }
 }
