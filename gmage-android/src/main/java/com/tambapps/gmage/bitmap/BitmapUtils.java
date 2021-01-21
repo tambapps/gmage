f3bd8be1bdaaeaeac30a9e80457c3951afe5fe2c
Nelson <nfonkoua@weborama.com>
2021-01-21 21:24:05 +0100

    added doc

--- gmage-android/src/main/java/com/tambapps/gmage/bitmap/BitmapUtils.java
+++ gmage-android/src/main/java/com/tambapps/gmage/bitmap/BitmapUtils.java
@@ -4,10 +4,18 @@ import android.graphics.Bitmap;
 import com.tambapps.gmage.Gmage;
 import com.tambapps.gmage.color.Color;
 
+/**
+ * Class containing useful functions to convert Bitmap from/to Gmage
+ */
 public final class BitmapUtils {
 
   private BitmapUtils() {}
 
+  /**
+   * Converts a Gmage to a Bitmap
+   * @param gmage the Gmage to convert
+   * @return the converted Bitmap
+   */
   public static Bitmap toBitmap(Gmage gmage) {
     int width = gmage.getWidth();
     int height = gmage.getHeight();
@@ -21,13 +29,18 @@ public final class BitmapUtils {
     return bmp;
   }
 
-  public static Gmage toGmage(Bitmap image) {
-    int width = image.getWidth();
-    int height = image.getHeight();
+  /**
+   * Converts a Bitmap to a Gmage
+   * @param bitmap the Bitmap to convert
+   * @return the converted Gmage
+   */
+  public static Gmage toGmage(Bitmap bitmap) {
+    int width = bitmap.getWidth();
+    int height = bitmap.getHeight();
     Color[] colors = new Color[width * height];
     for (int y = 0; y < height; y++) {
       for (int x = 0; x < width; x++) {
-        int argb = image.getPixel(x, y);
+        int argb = bitmap.getPixel(x, y);
         int alpha = argb >>> 24;
         int rgb = argb & 0xffffff;
         colors[y * width + x] = new Color(rgb, alpha);
