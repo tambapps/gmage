ba823206a359ec4c377a00d3964b8ca745d4a26c
Nelson <nfonkoua@weborama.com>
2021-01-23 00:50:18 +0100

    improved update script

--- update_javadoc.sh
+++ update_javadoc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git checkout main
+git checkout main || exit 1 > /dev/null
 mvn javadoc:javadoc || exit 1
 
 mkdir -p javadoc-temp/gmage-core/
@@ -12,13 +12,16 @@ cp -r gmage-desktop/target/site/apidocs/* javadoc-temp/gmage-desktop/
 
 cp README.md README.temp.md
 
-git checkout gh-pages
+git checkout gh-pages || exit 1 > /dev/null
 
 rm -r javadoc/
 mv javadoc-temp/ javadoc/
 
 rm -r README.md
 mv README.temp.md README.md
+
+# don't know why it doesn't delete gmage directories
+rm -r -f gmage-*
 
 git add README.md javadoc/
 echo "Now commit me!"
